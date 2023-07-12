dbb69e33925a559396b01723bdf765b06e29e559
Mikhail Novikau <melmenx@gmail.com>
2023-07-13 00:22:08 +0300

    Fix Authentication options

--- Departments/Program.cs
+++ Departments/Program.cs
@@ -11,57 +11,54 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<CompanyContext>(options =>
-    options.UseMySql(builder.Configuration.GetConnectionString("MySqlConnection")!,
+builder.Services.AddDbContext<CompanyContext>(options => options.UseMySql(builder.Configuration.GetConnectionString("MySqlConnection")!,
     ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("MySqlConnection"))));
-builder.Services.AddDefaultIdentity<ApplicationUserModel>(options =>
-    options.SignIn.RequireConfirmedAccount = true)
+
+builder.Services.AddDefaultIdentity<ApplicationUserModel>(options => options.SignIn.RequireConfirmedAccount = true)                
                 .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<CompanyContext>();
 
-builder.Services.AddMvc();
+
 builder.Services.AddScoped<UserManager<ApplicationUserModel>>();
-builder.Services.AddAuthentication(options =>
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+  .AddCookie(options =>
+   {
+     options.LoginPath = "/Account/Login";
+     options.LogoutPath = "/Account/Logout";
+     options.AccessDeniedPath = "/Account/Login";
+   });
+builder.Services.AddAuthorization(options =>
 {
-    options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-    options.DefaultSignOutScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-})
-    .AddCookie(options =>
-    {
-        options.LoginPath = "/Account/Login";
-        options.LogoutPath = "/Account/Logout";
-
-    });
-builder.Services.AddMemoryCache();
-
-var smtpSettings = builder.Configuration.GetSection("SmtpSettings").Get<SmtpSettings>();
-builder.Services.AddSingleton(smtpSettings);
-
-builder.Services.AddControllersWithViews(options =>
-{
-    options.Filters.Add<CheckUserExistFilter>();
+  options.AddPolicy("MyPolicy", policy =>
+  {
+    policy.RequireRole("User");
+  });
 });
 
-
-
+builder.Services.AddMemoryCache();
+builder.Services.AddControllersWithViews(options =>
+{
+  options.Filters.Add<CheckUserExistFilter>();
+});
+builder.Services.AddMvc();
+var smtpSettings = builder.Configuration.GetSection("SmtpSettings").Get<SmtpSettings>();
+builder.Services.AddSingleton(smtpSettings);
 builder.Services.AddTransient<IEmailSender, MailKitEmailSender>();
-
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+  app.UseExceptionHandler("/Home/Error");
+  // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+  app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
 app.UseAuthentication();
 app.UseAuthorization();
 
